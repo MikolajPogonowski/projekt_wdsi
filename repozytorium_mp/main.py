51d0307f40dfdc77c83fd2e38e109f35325e494f
MikolajPogonowski <96240055+MikolajPogonowski@users.noreply.github.com>
2022-02-11 17:16:51 +0100

    commit
    + learn_bowv
    + extract_features
    + train

--- repozytorium_mp/main.py
+++ repozytorium_mp/main.py
@@ -5,8 +5,10 @@ import shutil
 import glob
 import re
 import cv2
+import random
 from xml.dom import minidom
 import xml.etree.ElementTree as ElementTree
+from sklearn.ensemble import RandomForestClassifier
 #import PIL.Image as Image
 import matplotlib.pyplot as plt
 import lxml.etree as etree
@@ -166,6 +168,11 @@ def import_data_train(path, set):
         amount_limits = 0
 
         filename = root.find('filename').text
+        width = int(root.find('size/width').text)  # pozyskanie szerokości zdjęcia
+        height = int(root.find('size/height').text)  # pozyskanie wysokości zdjęcia
+        # print(width, height)
+
+        filename = root.find('filename').text
 
         obj = element.findall('object/name')
         dict = {}
@@ -184,36 +191,50 @@ def import_data_train(path, set):
 
         dict['image'] = images_list[n]  # dodawanie ściezki do obrazka
         dict['annotation'] = annotations_list[n]    # dodawanie ścieżki do xml
+        dict['height'] = height
+        dict['width'] = width
         dict['signs_number'] = amount_all
         dict['limits_number'] = amount_limits
 
         # dodawanie labela - czy na zdjęciu występuje speedlimit
-        if (flag == 1):
-            dict['label'] = 1
-            #data_list.append({'image': images_list[n], 'annotation': annotations_list[n], 'label': 1, 'signs_number': amount_all, 'limits_number': amount_limits})
-        else:
-            dict['label'] = 0
-            #data_list.append({'image': images_list[n], 'annotation': annotations_list[n], 'label': 0, 'signs_number': amount_all, 'limits_number': amount_limits})
+        # if (flag == 1):
+        #     dict['limit_label'] = 1
+        #     #data_list.append({'image': images_list[n], 'annotation': annotations_list[n], 'label': 1, 'signs_number': amount_all, 'limits_number': amount_limits})
+        # else:
+        #     dict['limit_label'] = 0
+        #     #data_list.append({'image': images_list[n], 'annotation': annotations_list[n], 'label': 0, 'signs_number': amount_all, 'limits_number': amount_limits})
 
         n += 1
 
-        all_signs = root.findall('object')
+        all_signs = root.findall('object')  # znajduje wszystkie znaki (objects) w obrębie JEDNEGO zdjęcia w xml
         #print(filename)
+
+        # dodawanie współrzędnych ramki ze znakiem speedlimit:
         j = 1
         for s in all_signs:
             name = s.find('name').text
             sign_frame = []
             if name == 'speedlimit':
-                xmin = int(s.find('bndbox/xmin').text)
-                dict[f'x_{j}_min'] = xmin
-                ymin = int(s.find('bndbox/ymin').text)
-                dict[f'y_{j}_min'] = ymin
-                xmax = int(s.find('bndbox/xmax').text)
-                dict[f'x_{j}_max'] = xmax
-                ymax = int(s.find('bndbox/ymax').text)
-                dict[f'y_{j}_max'] = ymax
+                dict[f'limit_flag{j}'] = 1
+            else:
+                dict[f'limit_flag{j}'] = 0
 
-                j += 1
+            xmin = int(s.find('bndbox/xmin').text)
+            dict[f'x_{j}_min'] = xmin
+            ymin = int(s.find('bndbox/ymin').text)
+            dict[f'y_{j}_min'] = ymin
+            xmax = int(s.find('bndbox/xmax').text)
+            dict[f'x_{j}_max'] = xmax
+            ymax = int(s.find('bndbox/ymax').text)
+            dict[f'y_{j}_max'] = ymax
+
+            # # print(xmin, ymin, xmax, ymax)
+            # img = cv2.imread(image_path)
+            # crop_img = img[ymin:ymax, xmin:xmax]
+            # # cv2.imshow("speedlimit", crop_img)
+            # # cv2.waitKey(0)
+
+            j += 1
 
                 #print(name, xmin, ymin, xmax, ymax)
 
@@ -265,29 +286,125 @@ def input_data():
 
     return 0
 
-def crop_speedlimits(database):
-    cropped_speedlimits = []
+def crop_signs(database):
+    cropped_signs = []
     for data in database:
         image_path = data['image']
-        print(image_path)
-
-        if data['label'] == 1:
-            for i in range(data['limits_number']):
-                xmin = data[f'x_{i+1}_min']
-                ymin = data[f'y_{i+1}_min']
-                xmax = data[f'x_{i+1}_max']
-                ymax = data[f'y_{i+1}_max']
-                #print(xmin, ymin, xmax, ymax)
-                img = cv2.imread(image_path)
-                crop_img = img[ymin:ymax, xmin:xmax]
-                #cv2.imshow("speedlimit", crop_img)
-                #cv2.waitKey(0)
-                cropped_speedlimits.append(crop_img)
-
-    return cropped_speedlimits
+        #print(image_path)
+        dict = []
 
 
-if __name__ == '__main__':
+        for i in range(data['signs_number']):
+            xmin = data[f'x_{i+1}_min']
+            ymin = data[f'y_{i+1}_min']
+            xmax = data[f'x_{i+1}_max']
+            ymax = data[f'y_{i+1}_max']
+            #print(xmin, ymin, xmax, ymax)
+            img = cv2.imread(image_path)
+            crop_img = img[ymin:ymax, xmin:xmax]
+            #cv2.imshow("speedlimit", crop_img)
+            #cv2.waitKey(0)
+
+            if data[f'limit_flag{i+1}'] == 1:
+                label = 1
+            else:
+                label = 0
+
+            # print(image_path)
+            # print(label)
+
+            cropped_signs.append({'image': crop_img, 'label': label})
+
+
+    return cropped_signs
+
+def sliding_window(image, stepSize, windowSize):    # windowSize = (width, height)
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            # xuất ra từng cửa sổ
+            yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+def crop_others(database):
+     cropped_others = []
+    # for data in database:
+    #     image_path = data['image']
+    #     print(image_path)
+    #
+    #     # współrzędne losowanej ramki (źle, bo czasem losuje xmax takie samo jak xmin i się wywala)!!!
+    #     xmin_o = random.randint(0, data['width'])
+    #     #x1 = xmin_o + 10
+    #     xmax_o = random.randint(xmin_o, data['width'])
+    #     ymin_o = random.randint(0, data['height'])
+    #     #y1 = ymin_o + 10
+    #     ymax_o = random.randint(ymin_o, data['height'])
+    #     #print(value)
+    #
+    #     img = cv2.imread(image_path)
+    #     crop_img = img[ymin_o:ymax_o, xmin_o:xmax_o]
+    #     #cv2.imshow("speedlimit", crop_img)
+    #     #cv2.waitKey(0)
+    #     cropped_others.append(crop_img)
+
+     return cropped_others
+
+def learn_bovw(data):
+    """
+    Learns BoVW dictionary and saves it as "voc.npy" file.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
+    @return: Nothing
+    """
+    dict_size = 128
+    bow = cv2.BOWKMeansTrainer(dict_size)
+
+    sift = cv2.SIFT_create()
+    for sample in data:
+        kpts = sift.detect(sample['image'], None)
+        kpts, desc = sift.compute(sample['image'], kpts)
+
+        if desc is not None:
+            bow.add(desc)
+
+    vocabulary = bow.cluster()
+
+    np.save('voc.npy', vocabulary)
+
+def extract_features(data):
+    """
+    Extracts features for given data and saves it as "desc" entry.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
+    @return: Data with added descriptors for each sample.
+    """
+    sift = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(sift, flann)
+    vocabulary = np.load('voc.npy')
+    bow.setVocabulary(vocabulary)
+    for sample in data:
+        kpts = sift.detect(sample['image'], None)
+        desc = bow.compute(sample['image'], kpts)
+        sample['desc'] = desc
+
+    return data
+
+def train(data):
+    """
+    Trains Random Forest classifier.
+    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image), "label" (class_id),
+                    "desc" (np.array with descriptor).
+    @return: Trained model.
+    """
+    descs = []
+    labels = []
+    for sample in data:
+        if sample['desc'] is not None:
+            descs.append(sample['desc'].squeeze(0))
+            labels.append(sample['label'])
+    rf = RandomForestClassifier()
+    rf.fit(descs, labels)
+
+    return rf
+
+def main():
     # print('Type classify: ')
     # operation = input()
     # if operation == 'c':
@@ -295,26 +412,32 @@ if __name__ == '__main__':
     # print(cv2.__version__)
 
     imported_train_data = import_data_train(path_ok, 'train')
+    for j in imported_train_data:
+        print(j)
 
-    cropped_limits = crop_speedlimits(imported_train_data)
+    cropped_signs = crop_signs(imported_train_data)
+    i = 0
+    x = 0
+    #for j in cropped_signs:
+        # cv2.imshow("speedlimit", j['image'])
+        # cv2.waitKey(0)
+        # print(j['label'])
+    #     x += int(j['label'])
+    #     i += 1
+    # print(i, x)
 
-    # for j in imported_train_data:
-    #     print(j)
+    learn_bovw(cropped_signs)
+    print('bowv_learned')
 
-    for j in cropped_limits:
-        cv2.imshow("speedlimit", j)
-        cv2.waitKey(0)
+    cropped_signs_1 = extract_features(cropped_signs)
+    print('features extracted')
 
-    #testowanie cv2.imread
-    # path_1 = r'D:\\AiR\\sem5\\WdSI lab\\projekt_wdsi\\train\\images\\road544.png'
-    # img = cv2.imread(path_1)
-    # crop_img = img[223:260, 107:145]
-    # #crop_img = img[1:100, 1:200]
-    # cv2.imshow("cropped", crop_img)
-    # cv2.waitKey(0)
+    RandomForest = train(cropped_signs_1)
+    print("model trained")
 
-    # sort_data()
-    # train_or_test_limits()
-    # train_or_test_others()
+
+
+if __name__ == '__main__':
+    main()
 
 
